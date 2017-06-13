2a0f76410d0db263d2cdbe3d2662e90392af2c09
Satoshi Matsushita <matsushi2009@gmail.com>
2017-06-13 00:54:43 -0700

    To run cron sudoer matsushi, but not needed ... maybe fullpath was needed. 	modified:   timerDaemon.sh

--- timerDaemon.sh
+++ timerDaemon.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -f
 
-php --php-ini /var/www/rfoutlet timerDaemon.php $* > /dev/null 2>&1
-echo TimerDaemon on $(date) >> Logs/TimerDaemon-sh.log
+# sudo sh -c "php --php-ini /var/www/rfoutlet timerDaemon.php > /dev/null 2>&1; 
+sudo sh -c "(cd /var/www/rfoutlet; echo -n \"TimerDaemon: $(date) :\"; \
+    php --php-ini /var/www/rfoutlet /var/www/rfoutlet/timerDaemon.php) >> \
+    /var/www/rfoutlet/Logs/TimerDaemon-sh.log 2>&1"
